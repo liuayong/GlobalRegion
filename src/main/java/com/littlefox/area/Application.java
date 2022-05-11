048e04e81dbde55d473c344782ab209f40eeb17f
刘阿勇 <liuayong@example.com>
2022-05-12 06:42:15 +0800

    国际化ResourceBundleMessageSource

--- src/main/java/com/littlefox/area/Application.java
+++ src/main/java/com/littlefox/area/Application.java
@@ -1,10 +1,12 @@
 package com.littlefox.area;
 
+import com.littlefox.area.config.MsgUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,7 +19,13 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    
+    
+    public Application(MessageSource messageSource) {
+        MsgUtil.inti(messageSource);
+    }
+    
+    
     @Configuration
     public class MyMvcConfig implements WebMvcConfigurer {
         @Override
