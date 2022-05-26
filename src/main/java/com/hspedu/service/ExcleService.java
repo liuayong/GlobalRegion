320a0cbf32f43e783d007e3a13ba1782bf811fba
qingquan <qingquan@inwil.com>
2022-05-26 15:12:27 +0800

    反射，excel操作

---
+++ src/main/java/com/hspedu/service/ExcleService.java
@@ -0,0 +1,21 @@
+package com.hspedu.service;
+
+import com.alibaba.excel.metadata.Sheet;
+
+import java.io.File;
+import java.util.List;
+
+public class ExcleService {
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\jun\\Desktop\\transfer\\11.xls";
+
+        File file = new File(filePath);
+        System.out.println(file.exists());
+
+
+        Sheet sheet = new Sheet(1, 1);
+        List<Object> objects = ExcelUtil.readLessThan1000Row(filePath,sheet);
+
+    }
+}
